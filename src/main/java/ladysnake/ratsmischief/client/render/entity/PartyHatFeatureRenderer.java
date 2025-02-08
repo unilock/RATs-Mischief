38bc5d7513a284296c3ba57aaff182971b71eab7
unilock <unilock@fennet.rentals>
2025-02-08 14:49:14 -0500

    statically initialize PartyHatFeatureRenderer.TEXTURES
    implements https://github.com/Ladysnake/RATs-Mischief/pull/158
    
    fixes https://github.com/Ladysnake/RATs-Mischief/issues/141
    fixes https://github.com/Ladysnake/RATs-Mischief/issues/142
    fixes https://github.com/Ladysnake/RATs-Mischief/issues/155
    
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/ladysnake/ratsmischief/client/render/entity/PartyHatFeatureRenderer.java
+++ src/main/java/ladysnake/ratsmischief/client/render/entity/PartyHatFeatureRenderer.java
@@ -18,6 +18,12 @@ import java.util.Set;
 public class PartyHatFeatureRenderer extends GeoRenderLayer<RatEntity> {
 	public static final Set<RatEntity.Type> DISALLOWED_TYPES = Sets.immutableEnumSet(RatEntity.Type.RAT_KID, RatEntity.Type.BIGGIE_CHEESE, RatEntity.Type.REMY);
 	public static Identifier[] TEXTURES;
+	static {
+		TEXTURES = new Identifier[RatEntity.PartyHat.values().length];
+		for (RatEntity.PartyHat hat : RatEntity.PartyHat.values()) {
+			TEXTURES[hat.ordinal()] = RatsMischief.id("textures/entity/birthday_hats/" + hat.toString().toLowerCase() + ".png");
+		}
+	}
 
 	private final PartyHatEntityRenderer partyHatEntityRenderer;
 
@@ -29,12 +35,6 @@ public class PartyHatFeatureRenderer extends GeoRenderLayer<RatEntity> {
 	@Override
 	public void render(MatrixStack poseStack, RatEntity animatable, BakedGeoModel bakedModel, RenderLayer renderType, VertexConsumerProvider bufferSource, VertexConsumer buffer, float partialTick, int packedLight, int packedOverlay) {
 		if (RatsMischiefUtils.IS_BIRTHDAY && !DISALLOWED_TYPES.contains(animatable.getRatType())) {
-			if (TEXTURES == null) {
-				TEXTURES = new Identifier[RatEntity.PartyHat.values().length];
-				for (RatEntity.PartyHat hat : RatEntity.PartyHat.values()) {
-					TEXTURES[hat.ordinal()] = RatsMischief.id("textures/entity/birthday_hats/" + hat.toString().toLowerCase() + ".png");
-				}
-			}
 			this.partyHatEntityRenderer.setTexture(TEXTURES[animatable.getPartyHat().ordinal()]);
 			this.partyHatEntityRenderer.render(
 				animatable,
