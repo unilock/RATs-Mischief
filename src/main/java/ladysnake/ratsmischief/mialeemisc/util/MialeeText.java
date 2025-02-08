72bc628f7196d3ce66971eb87fdfa3a0e67d35bd
unilock <unilock@fennet.rentals>
2025-02-08 14:24:31 -0500

    small optimizations
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/ladysnake/ratsmischief/mialeemisc/util/MialeeText.java
+++ src/main/java/ladysnake/ratsmischief/mialeemisc/util/MialeeText.java
@@ -1,6 +1,5 @@
 package ladysnake.ratsmischief.mialeemisc.util;
 
-import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 
 import java.util.List;
@@ -10,8 +9,7 @@ public class MialeeText {
 	 * Takes a text and returns the same text but with the given int color.
 	 */
 	public static Text withColor(Text text, int color) {
-		Style style = text.getStyle().withColor(color);
-		List<Text> styled = text.getWithStyle(style);
+		List<Text> styled = text.getWithStyle(text.getStyle().withColor(color));
 		if (!styled.isEmpty()) {
 			return styled.get(0);
 		}
--- src/main/java/ladysnake/ratsmischief/mixin/EnchantmentHelperMixin.java
+++ src/main/java/ladysnake/ratsmischief/mixin/EnchantmentHelperMixin.java
@@ -2,6 +2,8 @@ package ladysnake.ratsmischief.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import ladysnake.ratsmischief.common.init.ModEnchantments;
 import ladysnake.ratsmischief.common.item.RatMasterArmorItem;
 import ladysnake.ratsmischief.common.item.RatMasterMaskItem;
@@ -10,26 +12,22 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin {
-	@Unique
-	private static Enchantment storedEnchantment;
-
 	@WrapOperation(method = "getPossibleEntries", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/Enchantment;isAvailableForRandomSelection()Z"))
-	private static boolean mischief$storeEnchants(Enchantment enchantment, Operation<Boolean> original) {
+	private static boolean mischief$storeEnchants(Enchantment enchantment, Operation<Boolean> original, @Share("storedEnchantment") LocalRef<Enchantment> storedEnchantment) {
 		boolean originalResult = original.call(enchantment);
 		if (originalResult) {
-			storedEnchantment = enchantment;
+			storedEnchantment.set(enchantment);
 		}
 		return originalResult;
 	}
 
 	@WrapOperation(method = "getPossibleEntries", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/EnchantmentTarget;isAcceptableItem(Lnet/minecraft/item/Item;)Z"))
-	private static boolean mischief$letTheEnchantDecide(EnchantmentTarget enchantmentTarget, Item item, Operation<Boolean> original) {
-		if (storedEnchantment == ModEnchantments.RAT_CURSE) {
+	private static boolean mischief$letTheEnchantDecide(EnchantmentTarget enchantmentTarget, Item item, Operation<Boolean> original, @Share("storedEnchantment") LocalRef<Enchantment> storedEnchantment) {
+		if (storedEnchantment.get() == ModEnchantments.RAT_CURSE) {
 			return item instanceof RatMasterArmorItem || item instanceof RatMasterMaskItem;
 		}
 		return original.call(enchantmentTarget, item);
